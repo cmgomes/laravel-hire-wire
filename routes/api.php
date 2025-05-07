41ec712a0adec58a7d2e973eb1a21ca8fa42daa1
Chris Gomes <cmgomes.es@gmail.com>
2025-05-07 09:35:19 -0300

    ajuste nas rotas da api

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountTypeController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::middleware('auth')->group(function () {
-    Route::get('api/account-types', [AccountTypeController::class, 'index']);
-    Route::post('api/account/store', [AccountController::class, 'store'])->name('api.new-account.store');
-    Route::post('api/deposit', [TransactionController::class, 'deposit'])->name('api.deposit');
-    Route::post('api/withdraw', [TransactionController::class, 'withdraw'])->name('api.withdraw');
-    Route::get('api/user/accounts', [AccountController::class, 'getUserAccounts'])->name('user.accounts');
-    Route::get('api/transactions/last-week', [TransactionController::class, 'getLastWeekTransactions'])->name('api.transactions.last-week');
+Route::middleware('auth:api')->group(function (){
+    Route::get('account-types', [AccountTypeController::class, 'index']);
+    Route::post('account/store', [AccountController::class, 'store'])->name('api.new-account.store');
+    Route::post('deposit', [TransactionController::class, 'deposit'])->name('api.deposit');
+    Route::post('withdraw', [TransactionController::class, 'withdraw'])->name('api.withdraw');
+    Route::get('user/accounts', [AccountController::class, 'getUserAccounts'])->name('user.accounts');
+    Route::get('transactions/last-week', [TransactionController::class, 'getLastWeekTransactions'])->name('api.transactions.last-week');
 });
